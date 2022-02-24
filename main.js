2890b0471640bb66587fb12ecfc25d01902c6ec5
madhankrishhk <84235355+madhankrishhk@users.noreply.github.com>
2022-02-24 19:17:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    mustache = loadImage('https://i.postimg.cc/sgHrKswx/mustache-black-mustache-png-clipart-thumbnail-removebg-preview.png')
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialised');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 30, 30);
+
+
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
+function gotPoses(results) {
+    if (results.length >
+        0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
+        console.log("nosex = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
